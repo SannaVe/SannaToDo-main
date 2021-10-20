87900d612c0505fe136d7fee8172bfc50894be33
Sanna <89966100+SannaVe@users.noreply.github.com>
2021-10-20 21:26:16 +0300

    changes3

--- ToDo_Sanna.js
+++ ToDo_Sanna.js
@@ -1,35 +1,36 @@
 //selectors
-const taskinput = document.querySelector('.task-input');
-const taskbutton = document.querySelector('.task-button');
-const tasklist = document.querySelector('.task-list');
+const taskInput = document.querySelector(".task-input");
+const taskButton = document.querySelector(".task-button");
+const taskList = document.querySelector(".task-list");
 
 //Event listeners
-taskbutton.addEventListener('click', addTask);
+taskButton.addEventListener("click", addTask);
 
 //Functions
 function addTask(event){
     event.preventDefault();
+}
 
-    const taskDiv =document.createElement('div');
-    taskDiv.classlist.add('task');
+    const taskDiv =document.createElement("div");
+    taskDiv.classlist.add("task");
 
-    const newTask = document.createElement('li');
-    newTask.innerText = 'Hi';
-    newTask.classlist.add('task-item');
+    const newTask = document.createElement("li");
+    newTask.innerText = "Hi";
+    newTask.classlist.add("task-item");
 
     taskDiv.appendChild(newTask);
 
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class="fas fa-check"></i>";
     completedButton.classList.add("complete-button");
     taskDiv.appendChild(completedButton);
 
-    const deletedButton = document.createElement('button');
-    deletedButton.innerHTML = '<i class="fas fa-check"></i>';
+    const deletedButton = document.createElement("button");
+    deletedButton.innerHTML = "<i class="fas fa-trash"></i>";
     deletedButton.classList.add("delete-button");
     taskDiv.appendChild(deletedButton);
 
     taskList.appendChild(taskDiv);
 
 
-}
+
